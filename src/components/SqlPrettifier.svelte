70ce0428ff20b68c7dd08d2af1bb7dba35530bd4
iomcr <>
2021-12-21 11:06:17 -0500

    general upgrades

--- src/components/SqlPrettifier.svelte
+++ src/components/SqlPrettifier.svelte
@@ -13,6 +13,37 @@
     {/if}
     <div class="row">
       <div class="col">
+      </div>
+      <div class="col form-group">
+        <div class="btn-group">
+          <div class="btn btn-primary" on:click|preventDefault|capture={format}>Format</div>
+          <div class="btn btn-primary" on:click|preventDefault|capture={minify}>Minify</div>
+          <div class="btn btn-primary" on:click|preventDefault|capture={clear}>Clear</div>
+          <div class="btn btn-primary"
+            disabled="{history.length === 0}"
+            class:disabled={history.length === 0}
+            on:click|preventDefault|capture={undo}
+          >Undo</div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="form-group">
+          <AceEditor
+            bind:value={value}
+            lang={aceConfig.lang}
+            theme="clouds_midnight"
+            width="100%"
+            height="512"
+            on:init={editorInit}
+            on:input={onEditorChange}
+          />
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
         <div class="form-group">
           <div class="dropdown dropdown-dark">
             <select class="form-control dropdown-select"
@@ -43,7 +74,20 @@
       <div class="col">
         <div class="form-group">
           <select class="form-control dropdown-select"
-            bind:value={formatterConfig.lang}
+            bind:value={formatterConfig.linesBetweenQueries}
+          >
+            {#each formatterOptions.linesBetweenQueries as option}
+              <option value={option.value}>
+                {option.text}
+              </option>
+            {/each}
+          </select>
+        </div>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <select class="form-control dropdown-select"
+            bind:value={formatterConfig.indent}
           >
             {#each formatterOptions.indent as option}
               <option value={option.value}>
@@ -53,41 +97,16 @@
           </select>
         </div>
       </div>
-      <div class="col">
-        <div class="btn-group">
-          <div class="btn btn-primary" on:click|preventDefault|capture={format}>Format</div>
-          <div class="btn btn-primary" on:click|preventDefault|capture={minify}>Minify</div>
-          <div class="btn btn-primary" on:click|preventDefault|capture={clear}>Clear</div>
-          <div class="btn btn-primary"
-            disabled="{history.length === 0}"
-            class:disabled={history.length === 0}
-            on:click|preventDefault|capture={undo}
-          >Undo</div>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        <div class="form-group">
-          <AceEditor
-            bind:value={value}
-            lang={aceConfig.lang}
-            theme="clouds_midnight"
-            width="100%"
-            height="512"
-            on:init={editorInit}
-            on:input={onEditorChange}
-          />
-        </div>
-      </div>
     </div>
   </div>
 </div>
 
 <script>
-  import sqlFormatter from 'sql-formatter'
+  // import sqlFormatter from 'sql-formatter'
+  import * as prettierSql from 'prettier-sql'
   import pick from 'lodash/pick'
   import AceEditor from 'svelte-ace-editor';
+  console.log({ prettierSql })
 
   if (process.browser) {
     require('brace');
@@ -117,10 +136,35 @@
       { value: '        ', text: '8 spaces' },
       { value: '\t', text: 'tab' },
     ],
+    linesBetweenQueries: [
+      { value: 0, text: '0 lines between queries' },
+      { value: 1, text: '1 lines between queries' },
+      { value: 2, text: '2 lines between queries' },
+      { value: 3, text: '3 lines between queries' },
+      { value: 4, text: '4 lines between queries' },
+    ],
   });
   let formatterConfig = {
     indent: '    ',
     lang: 'sql',
+    linesBetweenQueries: 2,
+    uppercase: true,
+    keywordPosition: "standard", //| "tenSpaceLeft" | "tenSpaceRight"
+    // newline: {
+    //  "mode": "always" | "itemCount" | "lineWidth" | "hybrid" | "never",
+    //  "itemCount"?: number // only used if newline.mode is itemCount or hybrid
+    // },
+    // breakBeforeBooleanOperator: bool,
+    // aliasAs": "always" | "select" | "never",
+    // tabulateAlias: boolean,
+    commaPosition: "after", // "before" | "after" | "tabular",
+    parenOptions: {
+      openParenNewline: false,
+      closeParenNewline: false,
+    },
+    // lineWidth: number,
+    denseOperators: true,
+    semicolonNewline: true,
   };
   let aceOptions = Object.freeze({
     lang: ['pgsql', 'mysql', 'sql', 'sqlserver'],
@@ -157,8 +201,10 @@
     pushHistory()
     try {
       err = null
-      const options = pick(formatterConfig, ['indent', 'lang'])
-      value = sqlFormatter.format(value, options) + '\n'
+      const options = {
+        ...formatterConfig,
+      }
+      value = prettierSql.format(value, options) + '\n'
       value = value
     } catch (err) {
       // eslint-disable-next-line
